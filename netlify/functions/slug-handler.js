458cf405f983a8dc81212bcbe8a8075824e82bd4
Che <typace@proton.me>
2026-06-22 11:24:45 +0800

    Update slug-handler.js

--- netlify/functions/slug-handler.js
+++ netlify/functions/slug-handler.js
@@ -1,18 +1,14 @@
-// netlify/functions/slug-handler.js
-const { createVercelMocks } = require('./vercel-adapter');
-// 引入原动态路由函数
-const originalHandler = require('../../api/blog/[slug]'); 
+// netlify/functions/slug-handler.mjs
+import { createVercelMocks } from './vercel-adapter.mjs';
+import originalHandler from '../../api/blog/[slug].js';
 
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   const { req, res, getResult } = createVercelMocks(event);
   
-  // Netlify 将 /blog/my-post 匹配到 /blog/* 时，slug 在 event.path 中
-  // 我们需要提取 slug 并注入到 req.query 中，以兼容 Vercel 的动态路由行为
+  // 提取动态 slug 参数注入到 query 中
   const pathParts = event.path.split('/');
-  const slug = pathParts[pathParts.length - 1];
-  req.query.slug = slug; 
+  req.query.slug = pathParts[pathParts.length - 1]; 
   
-  // 如果原代码依赖 req.query 中的其他参数，也可以在这里补充
   await originalHandler(req, res);
   return getResult();
 };
